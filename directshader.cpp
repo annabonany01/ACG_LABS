3f54418db5dd8bc3dfd021a240a7215aa47418cf
asanchez0001 <114588217+asanchez0001@users.noreply.github.com>
2022-10-10 14:04:27 +0200

    Add files via upload

---
+++ directshader.cpp
@@ -0,0 +1,48 @@
+#include "directshader.h"
+#include "../core/utils.h"
+#include <iostream>
+
+
+DirectShader::DirectShader(const Vector3D& bgColor_):
+    Shader(bgColor_)
+{ }
+
+Vector3D DirectShader::computeColor(const Ray &r, const std::vector<Shape*> &objList, const std::vector<PointLightSource> &lsList) const
+{
+    Intersection its;
+    Vector3D new_color = Vector3D(0.0);
+
+    if (Utils::getClosestIntersection(r, objList, its))
+    {
+        if (its.shape->getMaterial().hasDiffuseOrGlossy())
+        {
+            Vector3D wo = (r.o - its.itsPoint).normalized();
+
+            for (const auto& light : lsList)
+            {
+                Vector3D wi = (light.getPosition() - its.itsPoint).normalized();
+                Vector3D Li = light.getIntensity(its.itsPoint);
+
+                Ray rr;
+                rr.o = its.itsPoint;
+                rr.d = wi;
+                //rr.depth = 0.0;
+                //rr.minT = 0.0001;
+                rr.maxT = (light.getPosition() - its.itsPoint).length();
+
+                if (!Utils::hasIntersection(rr, objList))
+                {
+
+                    Vector3D wr = its.shape->getMaterial().getReflectance(its.normal, wo, wi);
+                    //std::cout << " " << wr; break;
+                    new_color += Utils::multiplyPerCanal(Li, wr);
+                }
+            }
+        }
+        return new_color;
+    }
+    else
+        return bgColor;
+}
+
+
